6d9362615d8f2f24b4d85093af4a6c83bd62668f
Claudio Campos <ccampos@endgame.com>
2013-09-27 15:33:39 -0400

    add view model

--- cal/drills/views.py
+++ cal/drills/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.http import HttpResponse
+from drills.models import Day
+
+def home(request):
+    day_list = Day.objects.order_by('-exercise_type')[:5]
+    output = ', '.join([p.exercise_type for p in day_list])
+    return HttpResponse(output)

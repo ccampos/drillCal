54d6bb2868c2811eff6f9a3ed0660b9b38112754
Claudio Campos <ccampos@endgame.com>
2013-09-27 15:54:45 -0400

    add south, does not work

--- cal/drills/models.py
+++ cal/drills/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Day(models.Model):
     exercise_type = models.CharField(max_length=30)
     exert_percent = models.IntegerField(default=85)
+    distance = models.FloatField(default=3.11)
